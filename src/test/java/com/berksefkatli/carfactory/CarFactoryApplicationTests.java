e9d10c9bac3d352748709123a48c7e4e3df3238e
Berk Şefkatli <berksefkatli@gmail.com>
2021-04-27 20:52:59 +0300

    Spring Boot project initialized.

---
+++ src/test/java/com/berksefkatli/carfactory/CarFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berksefkatli.carfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
